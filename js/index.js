273938eead26020ed6a562e8b04d99c673f77dc9
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2023-05-19 11:05:05 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
-import LocomotiveScroll from "locomotive-scroll";
+import Lenis from "@studio-freight/lenis";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-let scrollEl = document.querySelector("[data-scroll-container]");
-
-const scroll = new LocomotiveScroll({
-  el: scrollEl,
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+  direction: "vertical", // vertical, horizontal
+  gestureDirection: "vertical", // vertical, horizontal, both
   smooth: true,
+  mouseMultiplier: 1,
+  smoothTouch: false,
+  touchMultiplier: 2,
+  infinite: false,
 });
+
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
