a70e7beff37eacf5aa2dd510ad7f8f7081d524d4
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2023-07-02 22:49:23 +0100

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,10 @@ import Lenis from "@studio-freight/lenis";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+// ----------------------------------------------------------------
+import { scrollbarComp } from "./components/scrollbar";
+scrollbarComp();
+
 const lenis = new Lenis({
   duration: 1.2,
   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
